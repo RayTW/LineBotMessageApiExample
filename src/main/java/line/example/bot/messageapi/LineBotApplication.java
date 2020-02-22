fc8d1fd104f4f10cac01787630c14db4884f6bb4
Ray <ray00000sina@gmail.com>
2020-02-22 20:59:04 +0800

    init.

---
+++ src/main/java/line/example/bot/messageapi/LineBotApplication.java
@@ -0,0 +1,27 @@
+package line.example.bot.messageapi;
+
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import org.springframework.boot.SpringApplication;
+
+public class LineBotApplication {
+
+  @EventMapping
+  public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+    final String originalMessageText = event.getMessage().getText();
+    return new TextMessage(originalMessageText);
+  }
+
+  @EventMapping
+  public void handleDefaultMessageEvent(Event event) {
+    System.out.println("event: " + event);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(LineBotApplication.class, args);
+  }
+}
