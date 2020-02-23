94f990e12c3b34da02a61abe79903cc32faad05f
Ray <ray00000sina@gmail.com>
2020-02-23 15:25:34 +0800

    fixed : 修正主程式未加上annation。

--- src/main/java/line/example/bot/messageapi/LineBotApplication.java
+++ src/main/java/line/example/bot/messageapi/LineBotApplication.java
@@ -6,8 +6,12 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+@LineMessageHandler
 public class LineBotApplication {
 
   @EventMapping
