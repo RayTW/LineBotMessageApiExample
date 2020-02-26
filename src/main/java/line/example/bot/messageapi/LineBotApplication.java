761f2c8602677570832227379ec8dcaba94704a7
Ray <ray00000sina@gmail.com>
2020-02-26 22:00:33 +0800

    fixed : 1.調整文字訊息跳行符號。 2.移除echo指令訊息前的面"\echo"。

--- src/main/java/line/example/bot/messageapi/LineBotApplication.java
+++ src/main/java/line/example/bot/messageapi/LineBotApplication.java
@@ -24,20 +24,19 @@ public class LineBotApplication {
   public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
     final String originalMessageText = event.getMessage().getText();
     if (originalMessageText.startsWith("/echo")) {
-      return new TextMessage("echo:" + originalMessageText);
+      return new TextMessage(originalMessageText.replace("/echo", ""));
     } else if (originalMessageText.startsWith("/傳說")) {
       return new TextMessage("傳說最新訊息: https://moba.garena.tw/news");
     } else if (originalMessageText.startsWith("/user")) {
       final String userId = event.getSource().getUserId();
       final StringBuilder txt = new StringBuilder();
-      final String newLine = "%0D%0A";
       LineBot.getInstance()
           .getInfo(userId)
           .ifPresent(
               info -> {
                 txt.append("使用者名稱:");
                 txt.append(info.getDisplayName());
-                txt.append(newLine);
+                txt.append(System.lineSeparator());
                 txt.append("使用者狀態:");
                 txt.append(info.getStatusMessage());
               });
